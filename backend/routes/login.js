515eced8ca9cff290aefd66e7fbeb8a7f38649a0
unknown <grigorischneider@gmail.com>
2023-02-01 11:21:49 +0100

    Login API Endpoint

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -19,4 +19,13 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
 
   if (!user) return res.status(400).send("Invalid email or password..");
+
+  const isValid = await bcrypt.compare(req.body.password, user.password);
+
+  if (!isValid) return res.status(400).send("Invalid email or password..");
+
+  const token = genAuthToken(user);
+  res.send(token);
 });
+
+module.exports = router;
